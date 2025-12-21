a98ccfcc716e0713ce17bec1be03a706ce1ba35a
Anton Kharin <aro.kharin@gmail.com>
2025-12-21 19:05:32 +0700

    Init

---
+++ build/tests/node_tests/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/illearo/repo/cmn/vlan_tagger/tests/node_tests
+# Build directory: /home/illearo/repo/cmn/vlan_tagger/build/tests/node_tests
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test([=[tagger_node_test]=] "/home/illearo/repo/cmn/vlan_tagger/build/tests/node_tests/tagger_node_test")
+set_tests_properties([=[tagger_node_test]=] PROPERTIES  _BACKTRACE_TRIPLES "/home/illearo/repo/cmn/vlan_tagger/tests/node_tests/CMakeLists.txt;14;add_test;/home/illearo/repo/cmn/vlan_tagger/tests/node_tests/CMakeLists.txt;0;")
